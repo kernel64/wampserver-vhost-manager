10cf0dbd31e0f1f83aa2f5e71303e219c013e21d
Mohamed Aymen Ben Slimane <med.aymen3@gmail.com>
2013-11-19 06:46:03 -0300

    validate empty fields

--- src/Mabs/WampVHostBundle/Controller/VhostController.php
+++ src/Mabs/WampVHostBundle/Controller/VhostController.php
@@ -7,69 +7,84 @@ use Symfony\Component\Validator\Constraints\NotBlank;
 
 class VhostController extends Controller
 {
+
     public function newAction()
     {
-    	$request = $this->getRequest();
+        $request = $this->getRequest();
         $form = $this->createFormBuilder()
-        ->add('servername', 'text', array(
-                                    'constraints' => new NotBlank()
-                                           ))
-        ->add('documentroot', 'text', array(
-                                    'constraints' => new NotBlank()
-                                           ))
-        ->getForm();
-    	if($request->isXmlHttpRequest() && $request->isMethod('POST')) {	
-    		$vHost = $this->get('mabs_wamp_v_host.manager');            
-                $form->handleRequest($request);
-                if ($form->isValid()) {              
-                    $data = $form->getData();     
-                    $vHost->createNewVHost($data['servername'], $data['documentroot']);
-	
-                    return $this->render('MabsWampVHostBundle:Vhost:empty.html.twig');      
-                }
-    	}
-    	
+                ->add('servername', 'text', array(
+                    'constraints' => new NotBlank()
+                ))
+                ->add('documentroot', 'text', array(
+                    'constraints' => new NotBlank()
+                ))
+                ->getForm();
+        if ($request->isXmlHttpRequest() && $request->isMethod('POST')) {
+            $vHost = $this->get('mabs_wamp_v_host.manager');
+            $form->handleRequest($request);
+            if ($form->isValid()) {
+                $data = $form->getData();
+                $vHost->createNewVHost($data['servername'], $data['documentroot']);
+
+                return $this->render('MabsWampVHostBundle:Vhost:empty.html.twig');
+            }
+        }
+
         return $this->render('MabsWampVHostBundle:Vhost:new.html.twig', array('form' => $form->createView()));
     }
-    
+
     public function updateAction($filename)
     {
-    	$request = $this->getRequest();
-    
-    	if(!$request->isXmlHttpRequest() && $request->isMethod('POST')) {
-    
-    		$vHost = $this->get('mabs_wamp_v_host.manager');
-    		$config    = $request->request->get('config');
-    
-    		$vHost->updateVHost($filename, $config);
-    
-    		return $this->redirect($this->generateUrl('homepage'), 301);
-    	}
-    	 
-    	return $this->render('MabsWampVHostBundle:Vhost:show.html.twig', array('filename' => $filename, 'data' => $config));
+        $request = $this->getRequest();
+
+        $form = $this->createFormBuilder()
+                ->add('config', 'textarea', array(
+                    'constraints' => new NotBlank()
+                ))
+                ->getForm();
+
+        if ($request->isXmlHttpRequest() && $request->isMethod('POST')) {
+
+            $form->handleRequest($request);
+            if ($form->isValid()) {
+                $data = $form->getData();
+
+                $vHost = $this->get('mabs_wamp_v_host.manager');
+                $vHost->updateVHost($filename, $data['config']);
+
+                return $this->render('MabsWampVHostBundle:Vhost:empty.html.twig');
+            }
+        }
+
+        return $this->render('MabsWampVHostBundle:Vhost:show.html.twig', array('filename' => $filename, 'form' => $form->createView()));
     }
-    
-    
+
     public function showAction($filename)
     {
-    	$vHost = $this->get('mabs_wamp_v_host.manager');
-    	$config = $vHost->showConfig($filename);
-    	
-    	return $this->render('MabsWampVHostBundle:Vhost:show.html.twig', array('filename' => $filename, 'data' => $config));
+        $vHost = $this->get('mabs_wamp_v_host.manager');
+        $data = array('config' => $vHost->showConfig($filename));
+        $form = $this->createFormBuilder($data)
+                ->add('config', 'textarea', array(
+                    'constraints' => new NotBlank()
+                ))
+                ->getForm();
+
+        return $this->render('MabsWampVHostBundle:Vhost:show.html.twig', array('filename' => $filename, 'form' => $form->createView()));
     }
-    
+
     public function deleteAction($filename)
     {
         $vHost = $this->get('mabs_wamp_v_host.manager');
         $config = $vHost->deleteVHost($filename);
-        
+
         return $this->listAction();
     }
-    
+
     public function listAction()
     {
-    	$vHost = $this->get('mabs_wamp_v_host.manager');
-    	
-    	return $this->render('MabsWampVHostBundle:Vhost:list.html.twig', array('vhosts' => $vHost->getList()));
+        $vHost = $this->get('mabs_wamp_v_host.manager');
+
+        return $this->render('MabsWampVHostBundle:Vhost:list.html.twig', array('vhosts' => $vHost->getList()));
     }
+
 }
